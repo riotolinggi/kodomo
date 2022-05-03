751719a358a05df1346194ccb51044b306c84a21
riotolinggi <103943795+riotolinggi@users.noreply.github.com>
2022-05-04 00:32:36 +0800

    Update karyawanService.js

--- frontend/src/helper/karyawanService.js
+++ frontend/src/helper/karyawanService.js
@@ -1,6 +1,6 @@
 import axiosJWT from "./axiosJWT";
 
-const API_URL = "http://localhost:5000/api/karyawan";
+const API_URL = "http://10.104.0.3:5000/api/karyawan";
 
 const getAllKaryawan = async (config) => {
   const response = await axiosJWT.get((config.API_URL = API_URL), config);
