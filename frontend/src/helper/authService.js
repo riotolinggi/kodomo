e4885cd3101ece11dc6f5eb3907d8082f407fa48
riotolinggi <103943795+riotolinggi@users.noreply.github.com>
2022-05-04 00:31:57 +0800

    Update authService.js

--- frontend/src/helper/authService.js
+++ frontend/src/helper/authService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/api/users/";
+const API_URL = "http://10.104.0.3:5000/api/users/";
 
 const axiosConfig = axios.create({
   baseURL: API_URL,
